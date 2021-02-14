3363fb269423082da91bedab4721642c07e7a3e4
npxrus <npxrus@yandex.com>
2021-02-14 09:51:42 +0300

    fead: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== "string" ||
+    sampleActivity === null ||
+    isNaN(sampleActivity)
+  ) {
+    return false;
+  }
+
+  if (
+    Number(sampleActivity) < 1 ||
+    Number(sampleActivity) > 8999 ||
+    Number(sampleActivity) == 15.1
+  ) {
+    return false;
+  }
+
+  let activity =
+    Math.log(MODERN_ACTIVITY / Number(sampleActivity)) /
+    (0.693 / HALF_LIFE_PERIOD);
+
+  return Math.ceil(activity);
 };
