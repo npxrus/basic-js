b8e9598f256095af286f1c80093dca850c2afc42
npxrus <npxrus@yandex.com>
2021-02-15 14:32:47 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,130 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machineType) {
+    this.machineType = machineType;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    let letters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    let arrMessage = message.toUpperCase().split("");
+    let arrKey = key.toUpperCase().split("");
+    let encryptedResult = [];
+
+    for (let i = 0, j = 0; i < arrMessage.length; i++, j++) {
+      if (letters.includes(arrMessage[i])) {
+        if (arrKey[j] === undefined) {
+          j = 0;
+        }
+
+        let sumEcnrypted =
+          arrMessage[i].charCodeAt() + arrKey[j].charCodeAt() - 65;
+
+        if (sumEcnrypted > 90) {
+          sumEcnrypted -= letters.length;
+        }
+
+        let encryptedLetter = String.fromCharCode(sumEcnrypted);
+        encryptedResult.push(encryptedLetter);
+      } else {
+        encryptedResult.push(arrMessage[i]);
+        j--;
+      }
+    }
+
+    if (this.machineType === false) {
+      return encryptedResult.reverse().join("");
+    } else return encryptedResult.join("");
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    let letters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    let arrMessage = message.toUpperCase().split("");
+    let arrKey = key.toUpperCase().split("");
+    let decryptedResult = [];
+
+    for (let i = 0, j = 0; i < arrMessage.length; i++, j++) {
+      if (letters.includes(arrMessage[i])) {
+        if (arrKey[j] === undefined) {
+          j = 0;
+        }
+
+        let diffDecrypted =
+          arrMessage[i].charCodeAt() - arrKey[j].charCodeAt() + 65;
+
+        if (diffDecrypted < 65) {
+          diffDecrypted = diffDecrypted + letters.length;
+        }
+
+        let decryptedLetter = String.fromCharCode(diffDecrypted);
+        decryptedResult.push(decryptedLetter);
+      } else {
+        decryptedResult.push(arrMessage[i]);
+        j--;
+      }
+    }
+
+    if (this.machineType === false) {
+      return decryptedResult.reverse().join("");
+    } else return decryptedResult.join("");
   }
 }
 
